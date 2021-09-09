405493ec94c20764c074e39d27140a9953255a39
Jimy Cox Rocha <terraxted@hotmail.com>
2021-09-08 22:36:47 -0500

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-btn depressed @click="cambiarEstado()">
+      Normal
+    </v-btn>
+
+    <SnackBar
+      v-if="activeNotification"
+      v-model="activeNotification"
+      :isError="isError"
+      :messages = "messages"
+    />
+
+
+  </v-container>
+</template>
+
+<script>
+import SnackBar from './SnackBar.vue'
+  export default {
+    name: 'HelloWorld',
+    data: () => ({
+      activeNotification: false,
+      isError: false,
+      messages: "I am pretty sure you misunderstood the question.merlin was asking if Vue would generate native DOM events after input value is changed through the v-model binding."
+    }),
+    methods: {
+      cambiarEstado(){
+        this.activeNotification = true;
+      }
+    },
+    components:{
+      SnackBar
+    }
+  }
+</script>
---
+++ SnackBar.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-snackbar
+        v-model="snackbar"
+        :multi-line="multiLine"
+        :timeout="timeout"
+        :color="(isError) ? 'error' : 'blue'"
+    >
+        {{ messages }}
+        <template v-slot:action="{ attrs }">
+        <v-btn
+            color='white'
+            text
+            v-bind="attrs"
+            @click = "childValueChangeEmit"
+        >
+            Close
+        </v-btn>
+        </template>
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+    name: "SnackBar",
+    data: () => ({
+        snackbar: true,
+        multiLine: true,
+        timeout: 2000,
+    }),
+    methods:{
+        childValueChangeEmit () {
+            this.snackbar = false;
+            this.$emit('input', this.snackbar)
+        }
+    },
+    props: {
+        value: Boolean,
+        isError: Boolean,
+        messages: String
+    },
+    watch: {
+        snackbar(){
+            this.$emit('input', this.snackbar)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
